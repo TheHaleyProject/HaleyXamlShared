183728c69582edb42a64da6fc3ad83636e2e226b
S Mahalingam <senguttuv.mahalingam@daep.ae>
2022-11-08 16:01:12 +0400

    Shared Libraries

---
+++ XamlShared/Models/CommonDictionary.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Haley.Models
+{
+    //Reason why we have it here separately is that Haley.MVVM doesn't have any reference to Haley.WPF.Resources but common dictionary is also needed in Haley.WPF.Resources.
+    //We do not need to maintain another separate nuget package for the utils that are shared between WPF/MVVM/Resources
+    //We need only to share a portion of the code between the two
+    internal class CommonDictionary : ResourceDictionary
+    {
+        public CommonDictionary() { }//need a new constructor
+
+        //A static dictionary
+        public static ConcurrentDictionary<Uri, ResourceDictionary> DictionaryStore = new ConcurrentDictionary<Uri, ResourceDictionary>();
+
+        private Uri _source;
+
+        public new Uri Source
+        {
+            get
+            {
+                //if (IsInDesignMode)
+                //{
+                //    return base.Source;
+                //}
+                return _source;
+            }
+            set
+            {
+                if (value == null) return;
+
+                //if (IsInDesignMode)
+                //{
+                //    base.Source = value;
+                //    return;
+                //}
+
+                //Instead of creating a new RD each time, we create and save the RD in a ConcurrentDictionary.
+                //So, next time some user control requests a RD for this same URI, we fetch the RD from our store and then add it to merged dictionaries. basically, we are having same dictionary referenced everywhere. The first benefit would be that if we change theme in one place, it should affect all places.
+                if (!DictionaryStore.ContainsKey(value))
+                {
+                    DictionaryStore.TryAdd(value, this);
+                    //And finally, we assign the source value to this resource dictionary.
+                    //We are also adding the value to the source to generate the dictionary
+                }
+                else
+                {
+                    ResourceDictionary result;
+                    DictionaryStore.TryGetValue(value, out result);
+                    MergedDictionaries.Add(result);
+                }
+                base.Source = value;
+                _source = value;
+
+            }
+        }
+
+    }
+}
---
+++ XamlShared/Utils/ResourceHelper.cs
@@ -0,0 +1,87 @@
+﻿using Haley.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media;
+using System.Diagnostics;
+using System.Resources;
+
+namespace Haley.Utils {
+    internal static class ResourceHelper {
+        public static CornerRadius cornerRadius = new CornerRadius(0.0);
+        public static Thickness borderThickness = new Thickness(1);
+
+        private static CommonDictionary colorDictionary;
+        private static List<CommonDictionary> imgSourceDics;
+        private static List<CommonDictionary> svgDics;
+
+        //[Conditional("HWPF")] //Conditional attribute works only for void returns
+        public static SolidColorBrush GetBrush(object resourceKey) {
+#if HWPF
+            if (colorDictionary == null)
+            {
+                //in case we decide to implement a Theme in Haley.WPF, we need to figure a way to change this theme at run time. (just like how we change theme using ThemeLoader)
+                colorDictionary = new CommonDictionary(); //Even though we use 'new' key word, common dictionary has a static dictionary store where it checks and returns only distinct value.
+                colorDictionary.Source = new Uri("pack://application:,,,/Haley.WPF;component/Dictionaries/ThemeColors/ThemeNormal.xaml",
+                            UriKind.Absolute);
+
+            }
+            if (colorDictionary.Contains(resourceKey))
+            {
+                return (SolidColorBrush)colorDictionary[resourceKey];
+            }
+#endif
+
+            return null;
+        }
+        public static ImageSource GetIcon(object resourceKey, IconTargetType type) {
+            //Haley WPF has references to Haley.WPF Resources and also to it's own resources (if any)
+
+            if (imgSourceDics == null || imgSourceDics.Count == 0) {
+                imgSourceDics = new List<CommonDictionary>();
+                imgSourceDics.Add(new CommonDictionary() { Source = new Uri("pack://application:,,,/Haley.WPF.Resources;component/Dictionaries/haleyIconsCommon.xaml", UriKind.RelativeOrAbsolute) });
+            }
+
+            if (svgDics == null || svgDics.Count == 0) {
+
+                svgDics = new List<CommonDictionary>();
+                svgDics.Add(new CommonDictionary() { Source = new Uri("pack://application:,,,/Haley.WPF.Resources;component/Dictionaries/haleySvgBranded.xaml", UriKind.RelativeOrAbsolute) });
+            }
+
+            ImageSource result = null;
+
+            //Try icons first, and then SVGS
+            if (type == IconTargetType.Any) {
+               result = GetIcon(imgSourceDics, resourceKey);
+                if (result == null) result = GetIcon(svgDics, resourceKey);
+            }
+
+            //Svgs
+            if (type == IconTargetType.Svg) {
+                result = GetIcon(svgDics, resourceKey);
+            }
+
+            //Imgs
+            if (type == IconTargetType.Image) {
+                result = GetIcon(imgSourceDics, resourceKey);
+            }
+            return result;
+        }
+
+        static ImageSource GetIcon(List<CommonDictionary> dicSource,object key) {
+            foreach (var dic in dicSource) {
+                if (dic.Contains(key)) {
+                    return dic[key] as ImageSource; //Sometimes imagesource could be null, if we try to return a different object.
+                }
+            }
+            return null;
+        }
+    }
+
+    internal enum IconTargetType {
+        Image,
+        Svg,
+        Any
+    }
+}
